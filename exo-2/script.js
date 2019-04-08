70dfb6bbd7aa12821d2ffc700a780e710f7ad87a
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-08 11:56:15 +0200

    Exo 2 selectionner terminus

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,14 @@
 console.log('exercice 2');
+$('#title');
+$('ul');
+$('footer');
+$('');
+$('body > div > div > div.col-sm-3.sidenav > div');
+$('span');
+$('.label-success');
+$('.intro');
+$('glyphicon-time');
+$('body > div > div > div.col-sm-3.sidenav > ul');
+$('p:odd');
 
+//3 principaux types de selecteurs : Tag Name, Tag ID, Tag Class.
