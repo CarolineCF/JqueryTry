00f5f28648f02f5cd5ec5b293b50c98e8613eacc
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-08 11:23:58 +0200

    Exo 1 commencement terminé

---
+++ exo-1/script.js
@@ -0,0 +1,8 @@
+console.log('exercice 1');
+/* Javascript est un langage de programmation alors que jQuery est un framework permettant de faciliter l'écriture en javascript.
+ jQuery a été écrit en JavaScript et est une bibliothèque à utiliser par JavaScript.*/
+
+ $(document).ready(function () {
+    console.log('Je peux maintenant écrire du code JQuery');
+
+});
