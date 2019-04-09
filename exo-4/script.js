f9e7a9fea270f8fe2e32c6a925f2ca55325117d9
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-09 17:00:09 +0200

    exo 4 en cours

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,1 +1,12 @@
 console.log("exercice 4");
+$(document).ready(() => {
+    $('.btn-danger').on('click', () => {
+        var essai = $('.form-control').val();
+      alert('Merci' + ' '+  essai + ' ' + 'nous vous tiendrons informé des différentes offres');
+      
+     
+    })
+    
+    
+    
+  }); 
