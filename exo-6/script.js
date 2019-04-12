a8020756e396c61498cd3c173bc4390b7e2ddc42
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-12 16:51:23 +0200

    recup info form + alert message

--- exo-6/script.js
+++ exo-6/script.js
@@ -1,1 +1,32 @@
 console.log("exercice 6");
+
+$(document).ready(() => {
+
+
+    var divAlert = ('#alertEmpty');
+    var divAlertPass = ('#alertPass');
+
+    $(divAlert).hide();
+    $(divAlertPass).hide();
+
+    $("#send").click(function () {
+        var emailUser = $('#inpEmail').val();
+        var passUser = $('#inpPass').val();
+
+        if ($("#inpEmail").val() === '') {
+
+            $(divAlert).show();
+        }
+
+        if ($("#inpPass").val() === '') {
+
+            $(divAlertPass).show();
+        }
+
+        console.log('Email de l\'utilisateur:' + ' ' + emailUser + ' | ' + 'Password de l\'utilisateur:' + ' ' + passUser);
+
+    });
+
+
+
+});
